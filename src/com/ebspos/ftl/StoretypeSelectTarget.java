513bc4afac866ab932f6ffd753ea82fa1e7ff286
gowish <gowish_rrrr@163.com>
2013-08-14 16:54:16 +0800

    仓库

---
+++ src/com/ebspos/ftl/StoretypeSelectTarget.java
@@ -0,0 +1,37 @@
+package com.ebspos.ftl;
+
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
+import java.util.Map;
+
+import com.ebspos.model.Goodstype;
+import com.ebspos.model.Storetype;
+
+import freemarker.core.Environment;
+import freemarker.template.TemplateDirectiveBody;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateModel;
+/**
+ * 组织机构选择
+ * @author loyin
+ * 2013-8-13
+ */
+public class StoretypeSelectTarget extends BaseTarget {
+	/**orgSelect*/
+	public static String targetName="storetypepSelect";
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void execute(Environment env, Map args, TemplateModel[] arg2,
+			TemplateDirectiveBody body) throws TemplateException, IOException {
+		List<Storetype> list= Storetype.dao.find("select id as id,type_name,type_code as num from storetype where ParentNo=0  order by id asc");
+		Writer out= env.getOut();
+		out.append("<option value='0'>一级类别</option>");
+		for(Storetype g:list){
+			out.append("<option value='"+g.getLong("id")+"'>");
+			out.append(g.getStr("name")+"["+g.getStr("num")+"]");
+			out.append("</option>");
+		}
+	}
+}
