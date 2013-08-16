980a288a2a779bbb8607efb9aea7bd3207471ac4
gowish <gowish_rrrr@163.com>
2013-08-16 16:42:16 +0800

    期初库存期表 初库存明细表（CKinitStoreDetail）

---
+++ src/com/ebspos/model/CKinitStore.java
@@ -0,0 +1,17 @@
+package com.ebspos.model;
+
+import net.loyin.jFinal.anatation.TableBind;
+
+import com.jfinal.plugin.activerecord.Model;
+
+/**
+ * 期初库存主表
+ * @author 熊涛
+ * 2013-08-15
+ */
+@TableBind(name="CKinitStore")
+public class CKinitStore extends Model<CKinitStore> {
+	private static final long serialVersionUID = 8855952602660614169L;
+	public static final CKinitStore dao=new CKinitStore();
+}
+
---
+++ src/com/ebspos/model/CKinitStoreDetail.java
@@ -0,0 +1,17 @@
+package com.ebspos.model;
+
+import net.loyin.jFinal.anatation.TableBind;
+
+import com.jfinal.plugin.activerecord.Model;
+
+/**
+ * 期初库存明细表
+ * @author 熊涛
+ * 2013-08-15
+ */
+@TableBind(name="CKinitStoreDetail")
+public class CKinitStoreDetail extends Model<CKinitStoreDetail> {
+	private static final long serialVersionUID = 6992124176791394802L;
+	public static final CKinitStoreDetail dao=new CKinitStoreDetail();
+}
+
