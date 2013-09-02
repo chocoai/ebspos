d14ef73a8084251f08999f827f7431a1dfa13036
gowish <gowish_rrrr@163.com>
2013-09-02 09:48:16 +0800

    仓库selector代码

--- src/com/ebspos/ftl/StoreSelectTarget.java
+++ src/com/ebspos/ftl/StoreSelectTarget.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.ebspos.model.Employee;
+import com.ebspos.model.Jbstore;
 
 import freemarker.core.Environment;
 import freemarker.template.TemplateDirectiveBody;
@@ -13,21 +14,21 @@ import freemarker.template.TemplateException;
 import freemarker.template.TemplateModel;
 /**
  * 员工选择
- * @author loyin
- * 2012-9-28
+ * @author 熊涛
+ * 2013-08-30
  */
-public class EmployeeSelectTarget extends BaseTarget {
-	/**empSelect*/
-	public static String targetName="empSelect";
+public class StoreSelectTarget extends BaseTarget {
+	/**storeSelect*/
+	public static String targetName="storeSelect";
 	@SuppressWarnings("rawtypes")
 	@Override
 	public void execute(Environment env, Map args, TemplateModel[] arg2,
 			TemplateDirectiveBody body) throws TemplateException, IOException {
-		List<Employee> list= Employee.dao.find("select id,usr_name,usr_no from employee ");
+		List<Jbstore> list= Jbstore.dao.find("select id,StoreCode,StoreName from jbstore ");
 		Writer out= env.getOut();
-		for(Employee o:list){
-			out.append("<option value='"+o.getStr("usr_no")+"'>");
-			out.append(o.getStr("usr_name")+"["+o.getStr("usr_no")+"]");
+		for(Jbstore o:list){
+			out.append("<option value='"+o.getLong("id")+"'>");
+			out.append(o.getStr("StoreName")+"["+o.getStr("StoreCode")+"]");
 			out.append("</option>");
 		}
 	}
